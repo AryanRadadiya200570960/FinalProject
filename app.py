cd83be1f780d03384bf324b35cb137b91f2d3739
AryanRadadiya200570960 <200570960@student.georgianc.on.ca>
2023-12-08 10:08:31 -0500

    main

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/google-charts/piechart')
+def google_piechart():
+    return render_template('PieChart.html')
+
 if __name__ == "__main__":
     app.run()   
