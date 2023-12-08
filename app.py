e8502d5c0fc6b7229495c3d8a9646f7a42e3ff87
AryanRadadiya200570960 <200570960@student.georgianc.on.ca>
2023-12-08 11:55:26 -0500

    main

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def index():
 def google_piechart():
     return render_template('PieChart.html')
 
+@app.route('/google-charts/linechart')
+def google_linechart():
+    return render_template('LineChart.html')
+
+@app.route('/google-charts/barchart')
+def google_barchart():
+    return render_template('BarChart.html')
+
 if __name__ == "__main__":
     app.run()   
